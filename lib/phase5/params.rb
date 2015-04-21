1e167fcdd5d3002a145c0a6501230a09af2b8d7b
rhoen <rhoen.pa@gmail.com>
2015-04-21 19:47:04 -0400

    initialize instance variables for request and response header

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,6 +10,7 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+
     end
 
     def [](key)
