6efeb249a7a86ab12144d47654e1897c17606b62
rhoen <rhoen.pa@gmail.com>
2015-04-22 09:47:29 -0400

    params obj handles non-nested query string

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -12,9 +12,11 @@ module Phase5
     def initialize(req, route_params = {})
       @req = req
       @route_params = route_params
+      parse_www_encoded_form(@req.query_string)
     end
 
     def [](key)
+      @params[key]
     end
 
     def to_s
@@ -30,8 +32,13 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
-      URI::decode_www_form(www_encoded_form)
-      byebug
+      @params = {}
+      return if www_encoded_form.nil?
+
+      query_pairs = URI::decode_www_form(www_encoded_form)
+      query_pairs.each do |pair|
+        @params[pair[0]] = pair[1]
+      end
     end
 
     # this should return an array
